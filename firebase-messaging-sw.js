a96e475abc034fba182e113a271c4811aded5d4e
adityataywade <taywadeaditya62@gmail.com>
2025-06-28 11:44:10 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -16,7 +16,7 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/ekd-logo.png'
+   icon: 'ekd-logo.png' 
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
